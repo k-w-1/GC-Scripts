58c5c8b03cc25b80228871f5b09ce3ed1de5ce38
Kevin <68672040+k-w-1@users.noreply.github.com>
2021-09-13 09:50:20 -0400

    Tweak for new URLs

--- Check-CDSJobs.ps1
+++ Check-CDSJobs.ps1
@@ -2,10 +2,10 @@ $HTML = New-Object -Com "HTMLFile"
 
 #Unknown reason (protected by CloudFlare?), but Invoke-WebRequest craps out for digital.canada.ca; below works a treat (and may even be faster?)
 #Later note: probably I just needed -UseBasicParsing for IWR, but this works anyway.
-$HTML.IHTMLDocument2_write((New-Object Net.WebClient).DownloadString("https://digital.canada.ca/join-our-team/"))
+$HTML.IHTMLDocument2_write((New-Object Net.WebClient).DownloadString("https://digital.canada.ca/careers/"))
 
 #correct the weird way the href is returned. Since $HTML is a COM object, it appears we need to give it a break between updating the href and copying to another object.
-$HTML.body.getElementsByClassName("job-posting-link") | % { $_.href = $CDS_baseurl + $_.href.trimStart("about:") } 
+$HTML.body.getElementsByClassName("job-posting-link") | % { $_.href = "https://digital.canada.ca" + $_.href.trimStart("about:") } 
 
 #get only the parts we want, and name the prop's better
 $list = $HTML.body.getElementsByClassName("job-posting-link") | select-object -property @{N='Title';E={$_.innerText}}, @{N='URL';E={$_.href}}
